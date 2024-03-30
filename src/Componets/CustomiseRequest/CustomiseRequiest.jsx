7e0ec0d56b51cb9077b4425e9dd944dc9636408c
sabiemuhammed <sabeeh@zinfog.com>
2024-03-30 12:08:17 +0530

    bugFix_inputFeilds

--- src/Componets/CustomiseRequest/CustomiseRequiest.jsx
+++ src/Componets/CustomiseRequest/CustomiseRequiest.jsx
@@ -16,7 +16,7 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 480,
+  width: 600,
   height: "98%",
   bgcolor: "background.paper",
   border: "none",
