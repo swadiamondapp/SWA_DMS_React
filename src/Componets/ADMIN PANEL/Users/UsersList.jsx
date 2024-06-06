39df819153736ca99fb94df2c9f7234e90c88b9c
sabiemuhammed <sabeeh@zinfog.com>
2024-06-06 10:45:49 +0530

    date_fix

--- src/Componets/ADMIN PANEL/Users/UsersList.jsx
+++ src/Componets/ADMIN PANEL/Users/UsersList.jsx
@@ -364,17 +364,10 @@ const UsersList = () => {
 
   const formatDate = (isoString) => {
     const date = new Date(isoString);
-    const options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      // second: 'numeric',
-      // timeZoneName: 'short',
-    };
-    return date.toLocaleString("en-US", options);
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
   };
 
   // update_user(setIsLoading, data, setUserList ,"70");
