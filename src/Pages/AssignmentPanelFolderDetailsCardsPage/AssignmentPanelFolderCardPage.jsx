ab73fa37d7c08a56a6cbc86566abb8f3043a18e6
Nithin2255 <nithin@zinfog.com>
2024-07-12 15:42:55 +0530

    changes

--- src/Pages/AssignmentPanelFolderDetailsCardsPage/AssignmentPanelFolderCardPage.jsx
+++ src/Pages/AssignmentPanelFolderDetailsCardsPage/AssignmentPanelFolderCardPage.jsx
@@ -1,3 +1,41 @@
+// import React, { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import Sidebar from "../../Componets/Sidebar/Sidebar";
+// import Header from "../../Componets/Header/Header";
+// import { list_designer_folderDetails } from "../../Componets/DESIGNER PANEL/Designer Detail View/Api";
+// import AssignmentPanelFolderCards from "../../Componets/Assignment Panel/AssignmentPanelFolderCards";
+// import RendersDetailPage from "../../Componets/Renders/RendersDetailPage/RendersDetailPage";
+
+// const DesignerAssignViewPage = () => {
+//   const { id } = useParams();
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [folderDetails, setFolderDetails] = useState([]);
+//   const [sidebarExpanded, setSidebarExpanded] = useState(true);
+
+//   useEffect(() => {
+//     list_designer_folderDetails(setIsLoading, setFolderDetails, id);
+//   }, []);
+
+//   return (
+//     <div className="DesignerDashboardPage">
+//       <Sidebar />
+//       <Header folderDetails={folderDetails} />
+//       <Sidebar
+//         sidebarExpanded={sidebarExpanded}
+//         setSidebarExpanded={setSidebarExpanded}
+//       />
+//       <Header folderDetails={folderDetails} sidebarExpanded={sidebarExpanded} />
+//       {/* <AssignmentPanelFolderCards
+//         folderDetails={folderDetails}
+//         id={id}
+//         sidebarExpanded={sidebarExpanded}
+//       /> */}
+//       <RendersDetailPage folderDetails={folderDetails} />
+//     </div>
+//   );
+// };
+
+// export default DesignerAssignViewPage;
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Sidebar from "../../Componets/Sidebar/Sidebar";
@@ -10,7 +48,6 @@ const DesignerAssignViewPage = () => {
   const { id } = useParams();
   const [isLoading, setIsLoading] = useState(false);
   const [folderDetails, setFolderDetails] = useState([]);
-  const [sidebarExpanded, setSidebarExpanded] = useState(true);
 
   useEffect(() => {
     list_designer_folderDetails(setIsLoading, setFolderDetails, id);
@@ -20,16 +57,7 @@ const DesignerAssignViewPage = () => {
     <div className="DesignerDashboardPage">
       <Sidebar />
       <Header folderDetails={folderDetails} />
-      <Sidebar
-        sidebarExpanded={sidebarExpanded}
-        setSidebarExpanded={setSidebarExpanded}
-      />
-      <Header folderDetails={folderDetails} sidebarExpanded={sidebarExpanded} />
-      {/* <AssignmentPanelFolderCards
-        folderDetails={folderDetails}
-        id={id}
-        sidebarExpanded={sidebarExpanded}
-      /> */}
+      {/* <AssignmentPanelFolderCards folderDetails={folderDetails} id={id} /> */}
       <RendersDetailPage folderDetails={folderDetails} />
     </div>
   );
--- src/Pages/Renders/RendersCardPage/RendersCardPage.jsx
+++ src/Pages/Renders/RendersCardPage/RendersCardPage.jsx
@@ -9,7 +9,6 @@ import RendersHome from "../../../Componets/Renders/RendersHome/RendersHome";
 const RendersPage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [designListData, setDesignListData] = useState([]);
-  const [sidebarExpanded, setSidebarExpanded] = useState(true);
 
   useEffect(() => {
     cadDesignList(setIsLoading, setDesignListData);
@@ -17,16 +16,11 @@ const RendersPage = () => {
 
   return (
     <div className="Parent_RendersCard">
-      <Sidebar
-        sidebarExpanded={sidebarExpanded}
-        setSidebarExpanded={setSidebarExpanded}
-      />
-      <Header sidebarExpanded={sidebarExpanded} />
-      <RenderCard
-        designListData={designListData}
-        sidebarExpanded={sidebarExpanded}
-      />
+      <Sidebar />
+      <Header />
+      {/* <RenderCard designListData={designListData} /> */}
       {/* <AnnotationCanvas /> */}
+      <RendersHome designListData={designListData} />
     </div>
   );
 };
