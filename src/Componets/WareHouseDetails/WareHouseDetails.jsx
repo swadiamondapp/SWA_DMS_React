2502b2254a61eaa9fd136cde4aa630a2c1d3d42e
sayyanZinfog01 <sayyan@zinfog.com>
2024-07-30 18:50:06 +0530

    changes

--- src/Componets/WareHouseDetails/WareHouseDetails.jsx
+++ src/Componets/WareHouseDetails/WareHouseDetails.jsx
@@ -1,10 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "../../Componets/WareHouseDetails/WareHouseDetails.css";
 import { LiaRupeeSignSolid } from "react-icons/lia";
-import image1 from "../../assets/wh_img.png";
-import image12 from "../../assets/ring_Wh.png";
-import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
   customization_details_view_warehouse,
   wareHouseEditBasicDetails,
@@ -14,7 +11,6 @@ import "react-toastify/dist/ReactToastify.css";
 import {
   diamond_type_dropdown_basicDetails,
   metal_type_dropdown_basicDetails,
-  product_category_basicDetails,
 } from "../Assignment Panel/Api";
 import { Select } from "antd";
 import SuccessModal from "../SuccessModal/SuccessModal";
@@ -31,7 +27,7 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
   const [successModalOpen, setSuccessModalOpen] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
   const [ErrorPriceMessage, setErrorPriceMessage] = useState(false);
-  const [actualFormData, setActualFormData] = useState({
+  const [formData, setFormData] = useState({
     length: "",
     width: "",
     height: "",
@@ -42,51 +38,42 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
     approxWeight: "",
     actualPrice: "",
   });
-  const [customizationwarehouseData, setCustomizationWareHouseData] = useState(
-    []
-  );
+  const [customizationwarehouseData, setCustomizationWareHouseData] = useState({});
 
-  const notify = () => toast("Wow so easy!");
-
-  // useEffect(() => {
-  //   if (customizationwarehouseData) {
-  //     setActualFormData((prevFormData) => ({
-  //       ...prevFormData,
-  //       SKU: customizationwarehouseData.customizationcode || "",
-  //       length: customizationwarehouseData.length_of_item || "",
-  //       width: customizationwarehouseData.width || "",
-  //       height: customizationwarehouseData.height || "",
-  //       typeOfMetal: customizationwarehouseData.metal_type || "",
-  //       diamondType: customizationwarehouseData.product_type || "",
-  //       approxDiamondWeight: customizationwarehouseData.diamond_weight || "",
-  //       approxWeight:customizationwarehouseData.weight||"",
-
-  //       // approxMetalWeights: basicDetails
-  //       //   ? basicDetails.approx_metal_weight
-  //       //   : "",
-  //       // approxMRP: basicDetails.approx_price || "",
-  //       // tag: basicDetails.tag || "",
-  //       // notes: basicDetails.notes || "",
-  //     }));
-  //   }
-  // }, [customizationwarehouseData]);
-  console.log(actualFormData, "formData");
-  console.log(wareHouseuserId, "wss");
   useEffect(() => {
     metal_type_dropdown_basicDetails(setMetalTypeDropDown);
     diamond_type_dropdown_basicDetails(setDiamondType);
   }, []);
+
   useEffect(() => {
-    customization_details_view_warehouse(
-      setIsLoading,
-      setCustomizationWareHouseData,
-      userWareHouseId
-    );
+    if (userWareHouseId) {
+      customization_details_view_warehouse(
+        setIsLoading,
+        setCustomizationWareHouseData,
+        userWareHouseId
+      );
+    }
   }, [userWareHouseId]);
+
+  useEffect(() => {
+    if (customizationwarehouseData) {
+      setFormData({
+        length: customizationwarehouseData.length_of_item || "",
+        width: customizationwarehouseData.width || "",
+        height: customizationwarehouseData.height || "",
+        notes: customizationwarehouseData.notes || "",
+        typeOfMetal: customizationwarehouseData.metal_type || [],
+        diamondType: customizationwarehouseData.diamond_type || [],
+        approxDiamondWeight: customizationwarehouseData.diamond_weight || "",
+        approxWeight: customizationwarehouseData.weight || "",
+        actualPrice: customizationwarehouseData.actual_price || "",
+      });
+    }
+  }, [customizationwarehouseData]);
 
   const handleInput = (e) => {
     const { name, value } = e.target;
-    setActualFormData((prevState) => ({
+    setFormData((prevState) => ({
       ...prevState,
       [name]: value,
     }));
@@ -95,23 +82,9 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
       [name]: "", // Clear the error for the current input field
     }));
   };
-  console.log(customizationwarehouseData, "customizationwarehouseData");
-
-  console.log(actualFormData, "actual");
 
   const handleUpdateWareHouseDetails = () => {
-    // toast("Please fill in the form", {
-    //   position: "top-right",
-    //   autoClose: 5000,
-    //   hideProgressBar: false,
-    //   closeOnClick: true,
-    //   pauseOnHover: true,
-    //   draggable: true,
-    //   progress: undefined,
-    //   theme: "light",
-    //   // transition: Bounce,
-    // });
-    if (!actualFormData.actualPrice) {
+    if (!formData.actualPrice) {
       setErrors((prevErrors) => ({
         ...prevErrors,
         actualPrice: "Actual price is required",
@@ -121,8 +94,8 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
 
     wareHouseEditBasicDetails(
       setIsLoading,
-      setActualFormData,
-      actualFormData,
+      setFormData,
+      formData,
       userWareHouseId,
       setSuccessMessage,
       setSuccessModalOpen,
@@ -131,15 +104,29 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
       navigate
     );
   };
+
   const onChange = (value) => {
     console.log(`selected ${value}`);
   };
+
   const onSearch = (value) => {
     console.log("search:", value);
   };
+
   const filterOption = (input, option) =>
     (option?.label ?? "").toLowerCase().includes(input.toLowerCase());
-  
+
+
+
+  const findMetalNameById = (id) => {
+    const item = MetalTypeDropDown.find((entry) => entry.id === id);
+    return item ? item.metal_name : "Not found";
+  };
+  const findDiamondNameById = (id) => {
+    const item = diamonType.find((entry) => entry.id === id);
+    return item ? item.name : "Not found";
+  };
+
   return (
     <>
       <div
@@ -178,40 +165,27 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                 </div>
                 <div className="A1_text">
                   <p>Type of metal</p>
-                  <p>{customizationwarehouseData.metal_type}</p>
+                  <p>{findMetalNameById(Number(customizationwarehouseData.metal_type))}</p>
                 </div>
                 <div className="A1_text">
                   <p>Diamond Type</p>
-                  <p>{customizationwarehouseData.diamond_type}</p>
+                  <p>{findDiamondNameById(Number(customizationwarehouseData.diamond_type))}</p>
                 </div>
                 <div className="A1_text">
                   <p>Approx Diamond weight</p>
                   <p>{customizationwarehouseData.diamond_weight} g</p>
                 </div>
-                {/* <div className="A1_text">
-                <p>Findings</p>
-                <p>Findings</p>
-              </div> */}
                 <div className="A1_text">
                   <p>Approx weight</p>
                   <p>{customizationwarehouseData.weight} g</p>
                 </div>
-
                 <div className="A1_text">
                   <p>Actual Price</p>
                   <p style={{ display: "flex", alignItems: "center" }}>
-                    {" "}
                     <LiaRupeeSignSolid />
                     {customizationwarehouseData.actual_price}
                   </p>
                 </div>
-                {/* <div className="A1_text">
-                <p>Tags</p>
-                <p>
-                  <span>Birthday</span>
-                  <span>Kids</span>
-                </p>
-              </div> */}
                 <div className="A1_text" style={{ borderBottom: "0px" }}>
                   <p>Note</p>
                   <p style={{ wordBreak: "break-word" }}>
@@ -233,13 +207,16 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                 </div>
                 <div className="A1_text">
                   <p>Length</p>
-                  <input
-                    type="number"
-                    className="actualDetails_input"
-                    name="length"
-                    value={actualFormData.length}
-                    onChange={handleInput}
-                  />
+                  <div className="">
+                    <input
+                      type="number"
+                      className="actualDetails_input"
+                      name="length"
+                      value={formData.length}
+                      onChange={handleInput}
+                    />
+                    mm
+                  </div>
                 </div>
                 <div className="A1_text">
                   <p>Width</p>
@@ -247,9 +224,8 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                     type="number"
                     className="actualDetails_input"
                     name="width"
-                    value={actualFormData.width}
+                    value={formData.width}
                     onChange={handleInput}
-                    // style={{backgroundColor:"red"}}
                   />
                 </div>
                 <div className="A1_text">
@@ -258,7 +234,7 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                     type="number"
                     className="actualDetails_input"
                     name="height"
-                    value={actualFormData.height}
+                    value={formData.height}
                     onChange={handleInput}
                   />
                 </div>
@@ -269,7 +245,7 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                     placeholder="-Select-"
                     optionFilterProp="children"
                     onChange={(value) =>
-                      setActualFormData((prevState) => ({
+                      setFormData((prevState) => ({
                         ...prevState,
                         typeOfMetal: value,
                       }))
@@ -277,29 +253,21 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                     onSearch={onSearch}
                     filterOption={filterOption}
                     style={{ width: "100%" }}
-                    // options={MetalTypeDropDown}
                     options={MetalTypeDropDown.map((item) => ({
                       value: item.id,
                       label: item.metal_name,
                     }))}
-                    value={customizationwarehouseData.metal_type}
+                    value={formData.typeOfMetal}
                   />
                 </div>
                 <div className="A1_text">
                   <p>Diamond Type</p>
-                  {/* <input
-                    type="number"
-                    className="actualDetails_input"
-                    name="diamondType"
-                    value={actualFormData.diamondType}
-                    onChange={handleInput}
-                  /> */}
                   <Select
                     showSearch
                     placeholder="-Select-"
                     optionFilterProp="children"
                     onChange={(value) =>
-                      setActualFormData((prevState) => ({
+                      setFormData((prevState) => ({
                         ...prevState,
                         diamondType: value,
                       }))
@@ -307,12 +275,11 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                     onSearch={onSearch}
                     filterOption={filterOption}
                     style={{ width: "100%" }}
-                    // options={MetalTypeDropDown}
                     options={diamonType.map((item) => ({
                       value: item.id,
                       label: item.name,
                     }))}
-                    value={customizationwarehouseData.diamond_type|| undefined}
+                    value={formData.diamondType}
                   />
                 </div>
                 <div className="A1_text">
@@ -321,63 +288,42 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                     type="number"
                     className="actualDetails_input"
                     name="approxDiamondWeight"
-                    value={actualFormData.approxDiamondWeight}
+                    value={formData.approxDiamondWeight}
                     onChange={handleInput}
                   />
                 </div>
-                {/* <div className="A1_text">
-                <p>Findings</p>
-                <input
-                  type="text"
-                  className="actualDetails_input"
-                  name="findings"
-                  value={actualFormData.findings}
-                  onChange={handleInput}
-                />
-              </div> */}
                 <div className="A1_text">
                   <p>Approx weight</p>
                   <input
                     type="number"
                     className="actualDetails_input"
                     name="approxWeight"
-                    value={actualFormData.approxWeight}
+                    value={formData.approxWeight}
                     onChange={handleInput}
                   />
                 </div>
                 <div className="A1_text" style={{ position: "relative" }}>
                   <p>Actual Price</p>
                   <p style={{ display: "flex", alignItems: "center" }}>
-                    {actualFormData.actualPrice.length > 0 && (
+                    {formData.actualPrice.length > 0 && (
                       <LiaRupeeSignSolid />
                     )}
-
                     <input
                       type="number"
                       className="actualDetails_input"
                       name="actualPrice"
-                      value={actualFormData.actualPrice}
+                      value={formData.actualPrice}
                       onChange={handleInput}
                     />
                   </p>
                 </div>
-                {/* <div className="A1_text">
-                <p>Tags</p>
-                <input
-                  type="text"
-                  className="actualDetails_input"
-                  name="tags"
-                  value={actualFormData.tags}
-                  onChange={handleInput}
-                />
-              </div> */}
                 <div className="A1_text" style={{ borderBottom: "0px" }}>
                   <p>Note</p>
                   <input
                     type="text"
                     className="actualDetails_input"
                     name="notes"
-                    value={actualFormData.notes}
+                    value={formData.notes}
                     onChange={handleInput}
                   />
                 </div>
@@ -387,7 +333,7 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                   </span>
                 )}
                 <div className="actualDetailsButtonContiner">
-                  <button onClick={() => handleUpdateWareHouseDetails()}>
+                  <button onClick={handleUpdateWareHouseDetails}>
                     Update
                   </button>
                   <ToastContainer
@@ -401,10 +347,7 @@ const WareHouseDetails = ({ sidebarExpanded }) => {
                     draggable
                     pauseOnHover
                     theme="light"
-                    // transition: Bounce,
                   />
-                  {/* Same as */}
-                  <ToastContainer />
                 </div>
               </div>
             </div>
