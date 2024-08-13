f7cb7ba7cb29127a0087534acce55b473f50678c
basithallied <43291406+basithallied@users.noreply.github.com>
2024-08-14 00:15:14 +0530

    Update ApiInstants.jsx

--- src/Pages/Services/ApiInstants.jsx
+++ src/Pages/Services/ApiInstants.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://dmsapi.swa.co/dms/";
+const BASE_URL = "https://livedmsapi.swa.co/dms/";
 
 export const apiService = axios.create({
   baseURL: BASE_URL,
