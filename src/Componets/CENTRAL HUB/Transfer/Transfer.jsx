9fffafa53e413d27c5e5ad23b2d363329bab74a0
nadira <nadira@zinfog.com>
2024-08-20 18:34:45 +0530

    changes

--- src/Componets/CENTRAL HUB/Transfer/Transfer.jsx
+++ src/Componets/CENTRAL HUB/Transfer/Transfer.jsx
@@ -277,7 +277,7 @@ const Transfer = ({ sidebarExpanded }) => {
                         }}
                       >
                         <div>
-                          <select
+                          {/* <select
                             className="scan_select_Central"
                             name="centralStatus"
                             id="centralHubStatus"
@@ -297,8 +297,8 @@ const Transfer = ({ sidebarExpanded }) => {
                           >
                             <option value="Created">Created</option>
                             <option value="Transfered">Transfered</option>
-                          </select>
-                          {/* <p
+                          </select> */}
+                          <p
                             style={{
                               color: "white",
                               background: "#0464D5",
@@ -307,8 +307,8 @@ const Transfer = ({ sidebarExpanded }) => {
                               borderRadius:"26px"
                             }}
                           >
-                            Transferred
-                          </p> */}
+                            {item.status}
+                          </p>
                         </div>
 
                         {/* <div>
