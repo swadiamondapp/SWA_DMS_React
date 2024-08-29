b5880e088882e3ac597a027068727d66bcb31985
sayyan <sayyan@zinfog.com>
2024-08-29 19:51:02 +0530

    changes

--- src/Pages/Services/ApiInstants.jsx
+++ src/Pages/Services/ApiInstants.jsx
@@ -10,7 +10,7 @@ apiService.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = `token ${token}`;
+      config.headers.Authorization = `token ${"e596f5c5513296ef1ac85579f80154db6505f009"}`;
     }
     return config;
   },
