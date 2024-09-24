533dcff33baf0cac775eee1715d5310014e82ef8
sayyan <sayyan@zinfog.com>
2024-09-24 13:10:12 +0530

    changes

--- src/Pages/CENTRAL HUB/CentralHubSlotPreview/PreviewTab.jsx
+++ src/Pages/CENTRAL HUB/CentralHubSlotPreview/PreviewTab.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import cross from "../../../assets/cross.png";
 import QRCodeGenerator from '../../../Componets/QRCodeGenerator/QRCodeGenerator';
 import { LuPrinter } from 'react-icons/lu';
@@ -8,6 +8,17 @@ import ReactToPrint, { useReactToPrint } from 'react-to-print';
 const PreviewTab = ({ sidebarExpanded, soltData }) => {
     
     const printRef = useRef();
+    const [isPrinting, setIsPrinting] = useState(false);
+    
+    const handlePrint = useReactToPrint({
+        content: () => printRef.current,
+        onBeforeGetContent: () => setIsPrinting(true),
+        onAfterPrint: () => {
+            setIsPrinting(false);
+            document.activeElement.blur();
+        },
+    });
+
     const formatDate = (isoString) => {
         const date = new Date(isoString);
         const day = String(date.getDate()).padStart(2, "0");
@@ -16,13 +27,26 @@ const PreviewTab = ({ sidebarExpanded, soltData }) => {
         return `${day}/${month}/${year}`;
     };
 
-    const handlePrint = useReactToPrint({
-        content: printRef.current,
-      });
+    const handleKeyDown = useCallback((event) => {
+        if (event.ctrlKey && event.key === 'p') {
+            event.preventDefault();
+            if (!isPrinting && printRef.current) {
+                handlePrint();
+            }
+        }
+    }, [handlePrint, isPrinting]);
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+        
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [handleKeyDown]);
 
     return (
         <div className="parentCentral" style={{ paddingLeft: sidebarExpanded ? "225px" : "130px" }}>
-            <div className="NewSlotPrint">
+            <div className="NewSlotPrint" ref={printRef}>
                 {soltData?.map((item, index) =>
                     item.caddesigns.map((design, designIndex) => (
                         <div className="slot_items" style={{ gap: "10px" }}>
@@ -34,18 +58,30 @@ const PreviewTab = ({ sidebarExpanded, soltData }) => {
 
                             <div className="" style={{ padding: "8px" }}>
                                 <div className="print_detail">
-                                    <div className="qrsection" style={{ paddingBottom: "98px" }}>
+                                    <div className="qrsection" >
                                         <div className="print_content" style={{ gap: "10px" }}>
                                             <span>Folder Name : {design.folder_name} </span>
                                             <span>Category : {design.product_category}</span>
                                             <span>Cad Name : {design.CAD_Name}</span>
                                             <span>Metal Type : {design.metal_type}</span>
+                                            <span>Tag : {design.Tag.map((tag, index) => (
+                                                <span key={index}>
+                                                    {tag}{index < design.Tag.length - 1 ? ' ,  ' : ''}
+                                                </span>
+                                            ))}</span>
+                                            <div style={{ width: "100%" }}>
+                                                <span>Findings :  {design.Findings.map((find, index) => (
+                                                    <span key={index}>
+                                                        {find}{index < design.Findings.length - 1 ? ' ,  ' : ''}
+                                                    </span>
+                                                ))}</span>
+                                            </div>
                                         </div>
-                                        <div className="print_qr">
+                                        <div className="print_qr" style={{marginLeft:"-35px"}}>
                                             <QRCodeGenerator value={design.designcode} />
                                         </div>
                                     </div>
-                                    <div className="img_section" style={{ height: "130px" }}>
+                                    <div className="img_section" style={{ height: "270px",width:"273px" }}>
                                         <img
                                             style={{ width: "100%", height: "100%" }}
                                             src={design.design_image}
@@ -55,10 +91,10 @@ const PreviewTab = ({ sidebarExpanded, soltData }) => {
                                     </div>
                                 </div>
 
-                                <div className="print_tag" style={{ gap: "10px", position: "relative", marginTop: "-12px", paddingBottom: "50px" }}>
+                                {/* <div className="print_tag" style={{ gap: "10px", position: "relative", marginTop: "-12px", paddingBottom: "50px" }}>
                                     <span>Tag : {design.Tag}</span>
                                     <span>Findings : {design.Findings}</span>
-                                </div>
+                                </div> */}
 
                                 <div className="approx_details" style={{ gap: "10px", position: "relative", marginTop: "17px" }}>
                                     <div className="approx" style={{ gap: "10px" }}>
@@ -66,7 +102,7 @@ const PreviewTab = ({ sidebarExpanded, soltData }) => {
                                             style={{
                                                 borderBottom: "1px solid black",
                                                 fontSize: "12px",
-                                                width: "35%",
+                                                width: "86px",
                                                 paddingBottom:"4px"
                                             }}
                                         >
@@ -136,6 +172,20 @@ const PreviewTab = ({ sidebarExpanded, soltData }) => {
                       />
                     </div>
             </div>
+            {/* <div style={{ width: "100%", display: "flex", justifyContent: "end", alignItems: "end", marginTop: "20px" }}>
+                <ReactToPrint
+                    trigger={() => (
+                        <div
+                            className="slotPrintButton"
+                            onClick={handlePrint}
+                            style={{ cursor: "pointer", padding: "8px 10px" }}
+                        >
+                            <LuPrinter /> Print
+                        </div>
+                    )}
+                    content={() => printRef.current}
+                />
+            </div> */}
         </div>
     )
 }
